71f2810770ac6bf71cd92b7574017dc0d04e5eb0
ThlD <madev.thld@gmail.com>
2016-11-19 21:13:52 +0300

    Правки для допуска

--- js/script.js
+++ js/script.js
@@ -10,6 +10,28 @@ var feedbackComment = popup.querySelector("[name=comment]");
 var form = popup.querySelector("form");
 var storageUsername = localStorage ? localStorage.getItem("feedbackUsername") : false;
 var storageEmail = localStorage ? localStorage.getItem("feedbackEmail") : false;
+var myMap;
+var myPlacemark;
+
+function init() {
+    myMap = new ymaps.Map('map', {
+        center: [59.939449, 30.327968],
+        zoom: 16
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+    
+    myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+        balloonContent: 'Магазин Gllacy'
+    },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map-marker.png',
+            iconImageSize: [218, 142],
+            iconImageOffset: [-45, -142]
+        });
+    myMap.geoObjects.add(myPlacemark);
+}
 
 link.addEventListener("click", function (event) {
     event.preventDefault();
@@ -52,3 +74,5 @@ overlay.addEventListener("click", function (event) {
     popup.classList.remove("feedback-form-show");
     overlay.classList.remove("overlay-show");
 });
+
+ymaps.ready(init);
